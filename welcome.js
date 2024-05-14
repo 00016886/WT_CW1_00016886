81513754e1f3bab957f3924940cbb65a6f66b7db
Asilbek0099 <156507579+Asilbek0099@users.noreply.github.com>
2024-05-14 21:17:25 +0500

    complete

--- welcome.js
+++ welcome.js
@@ -1,3 +1,26 @@
 window.onload = function() {
     alert("Welcome to TSS Fuel Cards!");
 };
+function validateForm() {
+    let name = document.forms["apply"]["name"].value;
+    if (name === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    let phone = document.forms["apply"]["phone"].value;
+    if (phone === "") {
+        alert("Put your phone number");
+        return false;
+    }
+
+    let company  = document.forms["apply"]["company"].value;
+    if (company === "") {
+        alert("Company name must be filled out");
+        return false;
+    }
+    let trucks = document.forms["apply"]["trucks"].value;
+    if (trucks === "") {
+        alert("Truck number must be shown");
+        return false;
+    }
+}
